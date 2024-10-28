0ef213ce06420902db4402f34f033beb4361d901
Josh Parkker <jparker@sherpas.ai>
2024-10-28 17:07:10 -0600

    variables

---
+++ introduction/variables/main.sh
@@ -0,0 +1,1 @@
+g++ ./variables.cpp -o out && ./out
---
+++ introduction/variables/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int player_health = 1000;
+
+    // don't touch below this line
+
+
+    cout << player_health << endl;
+}
+

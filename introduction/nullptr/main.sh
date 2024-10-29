aca016249ced8aeb9d3f3bf1b5cd8547c220a9b3
Josh Parkker <jparker@sherpas.ai>
2024-10-28 18:34:45 -0600

    nullptr

---
+++ introduction/nullptr/main.sh
@@ -0,0 +1,1 @@
+g++ ./nullptr.cpp -o out && ./out
---
+++ introduction/nullptr/nullptr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // create the empty "enemy" variable here
+    nullptr_t enemy = nullptr;
+
+    // don't touch below this line
+    cout << (enemy == nullptr) << endl;
+}
+
